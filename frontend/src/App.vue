e93887356b42f46bde89dbbdc5dcf2378da85a38
Даниил Певзнер <pevzner.daniil@gmail.com>
2023-06-16 07:58:53 +0200

    decomposed to make navbar a separate component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,18 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <router-link class="navbar-brand" to="/">My posts</router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <navbar></navbar>
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li v-if="!isAuthenticated" class="nav-item active">
-            <router-link class="nav-link" to="/sign-up">Sign Up <span class="sr-only">(current)</span></router-link>
-          </li>
-          <li v-if="!isAuthenticated" class="nav-item active">
-            <router-link class="nav-link" to="/log-in">Login</router-link>
-          </li>
-           <li v-if="isAuthenticated" class="nav-item active">
-            <a style="cursor: pointer" @click="logOut" class="nav-link">Log out</a>
-          </li>
-
-        </ul>
-<!--        <form class="form-inline my-2 my-lg-0">-->
-<!--          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">-->
-<!--          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-->
-<!--        </form>-->
-      </div>
-    </nav>
   <div class="app">
     <router-view></router-view>
   </div>
 </template>
 <script>
 import axios from 'axios'
-
+import navbar from "@/components/Navbar.vue";
 export default {
   name: 'App',
+  components: {
+    navbar
+  },
   beforeCreate() {
     this.$store.commit('initializeStore')
 
@@ -44,30 +24,7 @@ export default {
       axios.defaults.headers.common['Authorization'] = ''
     }
   },
-  methods: {
 
-
-  async logOut () {
-   try {
-        const response = await axios.post('/auth/token/logout/');
-        console.log(response)
-        if (response.status === 204) {
-          this.$store.commit('removeToken')
-          axios.defaults.headers.common['Authorization'] = ''
-          localStorage.clear()
-          this.$router.push('log-in')
-        }
-      } catch (e) {
-        console.log(e)
-        alert('Error')
-      }}
-  },
-  computed: {
-    isAuthenticated() {
-      console.log(this.$store.state.isAuthenticated)
-      return this.$store.state.isAuthenticated;
-    },
-  },
 }
 
 </script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <router-link class="navbar-brand" to="/">My posts</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li v-if="!isAuthenticated" class="nav-item active">
+            <router-link class="nav-link" to="/sign-up">Sign Up <span class="sr-only">(current)</span></router-link>
+          </li>
+          <li v-if="!isAuthenticated" class="nav-item active">
+            <router-link class="nav-link" to="/log-in">Login</router-link>
+          </li>
+           <li v-if="isAuthenticated" class="nav-item active">
+            <a style="cursor: pointer" @click="logOut" class="nav-link">Log out</a>
+          </li>
+
+        </ul>
+<!--        <form class="form-inline my-2 my-lg-0">-->
+<!--          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">-->
+<!--          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-->
+<!--        </form>-->
+      </div>
+    </nav>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from "axios"
+export default {
+  name: "navbar",
+   methods: {
+
+  async logOut () {
+   try {
+        const response = await axios.post('/auth/token/logout/');
+        console.log(response)
+        if (response.status === 204) {
+          this.$store.commit('removeToken')
+          axios.defaults.headers.common['Authorization'] = ''
+          localStorage.clear()
+          this.$router.push('log-in')
+        }
+      } catch (e) {
+        console.log(e)
+        alert('Error')
+      }}
+  },
+  computed: {
+    isAuthenticated() {
+      console.log(this.$store.state.isAuthenticated)
+      return this.$store.state.isAuthenticated;
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
